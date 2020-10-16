e683825cea81834f8e254100d6c40e38dc0fa5c6
mdibuhossain <mohammad.ihakash@gmail.com>
2020-10-17 03:46:56 +0600

    first commit

---
+++ Numbers_on_Whiteboard.cpp
@@ -0,0 +1,88 @@
+/*
+ ██▓ ▄▄▄▄    ██▀███   ▄▄▄       ██░ ██  ██▓ ███▄ ▄███▓
+▓██▒▓█████▄ ▓██ ▒ ██▒▒████▄    ▓██░ ██▒▓██▒▓██▒▀█▀ ██▒
+▒██▒▒██▒ ▄██▓██ ░▄█ ▒▒██  ▀█▄  ▒██▀▀██░▒██▒▓██    ▓██░
+░██░▒██░█▀  ▒██▀▀█▄  ░██▄▄▄▄██ ░▓█ ░██ ░██░▒██    ▒██ 
+░██░░▓█  ▀█▓░██▓ ▒██▒ ▓█   ▓██▒░▓█▒░██▓░██░▒██▒   ░██▒
+░▓  ░▒▓███▀▒░ ▒▓ ░▒▓░ ▒▒   ▓▒█░ ▒ ░░▒░▒░▓  ░ ▒░   ░  ░
+ ▒ ░▒░▒   ░   ░▒ ░ ▒░  ▒   ▒▒ ░ ▒ ░▒░ ░ ▒ ░░  ░      ░
+ ▒ ░ ░    ░   ░░   ░   ░   ▒    ░  ░░ ░ ▒ ░░      ░   
+ ░   ░         ░           ░  ░ ░  ░  ░ ░         ░   
+          ░                                           
+*/
+//#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <iterator>
+#include <bitset>
+#include <assert.h>
+#include <new>
+#include <sstream>
+#include <time.h>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+#ifndef ONLINE_JUDGE
+    clock_t tStart = clock();
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, avg, a, b;
+        cin >> n;
+        cout << 2 << endl;
+        if (n == 2)
+            cout << 1 << " " << 2 << endl;
+        else
+        {
+            for (ll i = 1; i < n; i++)
+            {
+                if (i == 1)
+                {
+                    b = n;
+                    a = n - 2;
+                    cout << a << " " << b << endl;
+                    b = (a + b) / 2;
+                }
+                else if (i == 2)
+                {
+                    a = n - 1;
+                    cout << a << " " << b << endl;
+                    b = (a + b) / 2;
+                }
+                else
+                {
+                    a = b - 2;
+                    cout << a << " " << b << endl;
+                    b = (a + b) / 2;
+                }
+            }
+        }
+    }
+
+#ifndef ONLINE_JUDGE
+    fprintf(stderr, "\nRuntime: %.10fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+#endif
+    return 0;
+}
