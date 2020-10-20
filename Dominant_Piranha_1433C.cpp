82e2f6ca14a39a8f092cf0b0e4708267928edc04
mdibuhossain <mohammad.ihakash@gmail.com>
2020-10-21 03:11:23 +0600

    first commit

---
+++ Dominant_Piranha_1433C.cpp
@@ -0,0 +1,96 @@
+/*
+ ██▓ ▄▄▄▄    ██▀███   ▄▄▄       ██░ ██  ██▓ ███▄ ▄███▓
+▓██▒▓█████▄ ▓██ ▒ ██▒▒████▄    ▓██░ ██▒▓██▒▓██▒▀█▀ ██▒
+▒██▒▒██▒ ▄██▓██ ░▄█ ▒▒██  ▀█▄  ▒██▀▀██░▒██▒▓██    ▓██░
+░██░▒██░█▀  ▒██▀▀█▄  ░██▄▄▄▄██ ░▓█ ░██ ░██░▒██    ▒██ 
+░██░░▓█  ▀█▓░██▓ ▒██▒ ▓█   ▓██▒░▓█▒░██▓░██░▒██▒   ░██▒
+░▓  ░▒▓███▀▒░ ▒▓ ░▒▓░ ▒▒   ▓▒█░ ▒ ░░▒░▒░▓  ░ ▒░   ░  ░
+ ▒ ░▒░▒   ░   ░▒ ░ ▒░  ▒   ▒▒ ░ ▒ ░▒░ ░ ▒ ░░  ░      ░
+ ▒ ░ ░    ░   ░░   ░   ░   ▒    ░  ░░ ░ ▒ ░░      ░   
+ ░   ░         ░           ░  ░ ░  ░  ░ ░         ░   
+          ░                                           
+*/
+//#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <iterator>
+#include <bitset>
+#include <assert.h>
+#include <new>
+#include <sstream>
+#include <time.h>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+#ifndef ONLINE_JUDGE
+    clock_t tStart = clock();
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, mx;
+        cin >> n;
+        ll ar[n];
+        bool tf = true;
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> ar[i];
+            if ((i > 0) && (ar[i] != ar[i - 1]))
+                tf = false;
+        }
+        if (tf)
+            cout << -1 << endl;
+        else
+        {
+            int j = 0;
+            mx = ar[0];
+            for (ll i = 0; i < n; i++)
+                if (ar[i] > mx)
+                {
+                    mx = ar[i];
+                    j = i;
+                }
+            for (ll i = j; 1; i++)
+            {
+                if ((ar[i] > ar[i - 1]) && i > 0)
+                {
+                    cout << i + 1 << endl;
+                    break;
+                }
+
+                else if (ar[i] > ar[i + 1])
+                {
+                    cout << i + 1 << endl;
+                    break;
+                }
+            }
+        }
+    }
+
+#ifndef ONLINE_JUDGE
+    fprintf(stderr, "\nRuntime: %.10fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+#endif
+    return 0;
+}
