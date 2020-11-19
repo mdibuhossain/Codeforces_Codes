66de6c27d9939a9ea03fb22430ce80a33cb5d0be
mdibuhossain <mohammad.ihakash@gmail.com>
2020-11-19 22:44:55 +0600

    first commit

---
+++ Robot_Program_1452A.cpp
@@ -0,0 +1,83 @@
+/*
+ ██▓ ▄▄▄▄    ██▀███   ▄▄▄       ██░ ██  ██▓ ███▄ ▄███▓
+▓██▒▓█████▄ ▓██ ▒ ██▒▒████▄    ▓██░ ██▒▓██▒▓██▒▀█▀ ██▒
+▒██▒▒██▒ ▄██▓██ ░▄█ ▒▒██  ▀█▄  ▒██▀▀██░▒██▒▓██    ▓██░
+░██░▒██░█▀  ▒██▀▀█▄  ░██▄▄▄▄██ ░▓█ ░██ ░██░▒██    ▒██ 
+░██░░▓█  ▀█▓░██▓ ▒██▒ ▓█   ▓██▒░▓█▒░██▓░██░▒██▒   ░██▒
+░▓  ░▒▓███▀▒░ ▒▓ ░▒▓░ ▒▒   ▓▒█░ ▒ ░░▒░▒░▓  ░ ▒░   ░  ░
+ ▒ ░▒░▒   ░   ░▒ ░ ▒░  ▒   ▒▒ ░ ▒ ░▒░ ░ ▒ ░░  ░      ░
+ ▒ ░ ░    ░   ░░   ░   ░   ▒    ░  ░░ ░ ▒ ░░      ░   
+ ░   ░         ░           ░  ░ ░  ░  ░ ░         ░   
+          ░                                           
+*/
+//#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <iterator>
+#include <bitset>
+#include <assert.h>
+#include <new>
+#include <sstream>
+#include <time.h>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+using namespace std;
+typedef long long int ll;
+
+ll funFactorial(ll n)
+{
+    if (n == 0)
+        return 1;
+    return n * funFactorial(n - 1);
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    clock_t tStart = clock();
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll x, y;
+        cin >> x >> y;
+        if (x == y)
+            cout << x + y << endl;
+        else if (x > y)
+        {
+            ll z = max(x, y);
+            z *= 2;
+            cout << z - 1 << endl;
+        }
+        else
+        {
+            ll z = max(x, y);
+            z *= 2;
+            cout << z - 1 << endl;
+        }
+    }
+
+#ifndef ONLINE_JUDGE
+    fprintf(stderr, "\nRuntime: %.10fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+#endif
+    return 0;
+}
