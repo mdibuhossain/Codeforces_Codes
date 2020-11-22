5392d7d7f1dd4e568916dfb771658a1cc658c9ae
mdibuhossain <mohammad.ihakash@gmail.com>
2020-11-23 05:00:31 +0600

    first commit

---
+++ Suits_1271A.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <iterator>
+#include <bitset>
+#include <assert.h>
+#include <new>
+#include <sstream>
+#include <time.h>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+using namespace std;
+typedef long long int ll;
+
+ll funFactorial(ll n)
+{
+    if (n == 0)
+        return 1;
+    return n * funFactorial(n - 1);
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    clock_t tStart = clock();
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+    if (f >= e)
+    {
+        ll cs = min(b, min(c, d));
+        d -= cs;
+        cs *= f;
+        cs += e * min(a, d);
+        cout << cs << endl;
+    }
+    else
+    {
+        ll cs = min(a, d);
+        d -= cs;
+        cs *= e;
+        cs += f * min(b, min(c, d));
+        cout << cs << endl;
+    }
+
+#ifndef ONLINE_JUDGE
+    fprintf(stderr, "\nRuntime: %.10fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+#endif
+    return 0;
+}
