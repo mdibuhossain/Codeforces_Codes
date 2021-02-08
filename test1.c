e39b2fb67998e451a803d61115f8617a93a17629
Ibrahim <mohammad.ihakash@gmail.com>
2021-02-08 14:50:27 +0600

    added

--- test1.c
+++ test1.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <string.h>
-#define ll long long int
-#define ui unsigned int
-#define true 1
-int main()
-{
-	int n = 5;
-
-	printf("%d\n", ++n); // n + 1, print //
-	// n = 6;
-	printf("%d\n", n++); // print , n + 1 // postincreament
-	// n = 7
-	printf("%d\n", n);
-	return 0;
-}
+=
